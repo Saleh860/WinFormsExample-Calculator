eb250bb0047ba4593744abf88a55b0a54db5a25b
ahdo1440 <s44452361@students.tu.edu.sa>
2024-04-17 09:32:53 +0300

    homework omar adel 44452361

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -11,13 +11,54 @@ namespace Calculator
         {
             try
             {
-                result.Text = $" = {
-                    (double.Parse(first.Text) + 
+                result.Text = $" = {(double.Parse(first.Text) +
                     double.Parse(second.Text))}";
-            } catch (FormatException)
+            }
+            catch (FormatException)
+            {
+                result.Text = "Error";
+            }
+        }
+
+        private void subtract_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                result.Text = $" = {(double.Parse(first.Text) -
+                    double.Parse(second.Text))}";
+            }
+            catch (FormatException)
+            {
+                result.Text = "Error";
+            }
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                result.Text = $" = {(double.Parse(first.Text) *
+                    double.Parse(second.Text))}";
+            }
+            catch (FormatException)
+            {
+                result.Text = "Error";
+
+            }
+        }
+
+        private void divide_Click(object sender, EventArgs e)
+        {
+        try
+            {
+                result.Text = $" = {(double.Parse(first.Text) /
+                    double.Parse(second.Text))}";
+            }
+            catch (FormatException)
             {
                 result.Text = "Error";
             }
         }
     }
 }
+   
