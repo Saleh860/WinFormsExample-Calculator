4391fdf2d3a624a727ad7f4879dc569150dd7868
Turki72 <Tourkix33@gmail.com>
2024-04-17 09:47:11 +0300

    H.W-1

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -1,23 +1,68 @@
 namespace Calculator
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
-        private void add_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                result.Text = $" = {
-                    (double.Parse(first.Text) + 
-                    double.Parse(second.Text))}";
-            } catch (FormatException)
-            {
-                result.Text = "Error";
-            }
-        }
-    }
+		private void add_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				result.Text = $" {(double.Parse(first.Text) +
+					double.Parse(second.Text))}";
+			}
+			catch (FormatException)
+			{
+				result.Text = "Error";
+			}
+
+		}
+
+		private void subtract_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				result.Text = $" {(double.Parse(first.Text) -
+					double.Parse(second.Text))}";
+			}
+			catch (FormatException)
+			{
+				result.Text = "Error";
+			}
+		}
+
+		private void milt(object sender, EventArgs e)
+		{
+			try
+			{
+				result.Text = $" {(double.Parse(first.Text) *
+					double.Parse(second.Text))}";
+			}
+			catch (FormatException)
+			{
+				result.Text = "Error";
+			}
+		}
+
+		private void devid(object sender, EventArgs e)
+		{
+			try
+			{
+				result.Text = $"{(double.Parse(first.Text) /
+					double.Parse(second.Text))}";
+			}
+			catch (FormatException)
+			{
+				result.Text = "Error";
+			}
+		}
+
+		private void submint(object sender, EventArgs e)
+		{
+			
+		}
+	}
 }
