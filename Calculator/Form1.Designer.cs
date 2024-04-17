056efa0ebdb7583a7b48f01597d4e1585aab3fa3
musab-8765 <mossapsendy1424@gmail.com>
2024-04-17 14:59:00 +0300

    musab  yasser edited calculator

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -43,25 +43,28 @@
             // first
             // 
             first.Font = new Font("Segoe UI", 14F);
-            first.Location = new Point(96, 21);
+            first.Location = new Point(110, 28);
+            first.Margin = new Padding(3, 4, 3, 4);
             first.Name = "first";
-            first.Size = new Size(100, 32);
+            first.Size = new Size(114, 39);
             first.TabIndex = 0;
             // 
             // second
             // 
             second.Font = new Font("Segoe UI", 14F);
-            second.Location = new Point(96, 70);
+            second.Location = new Point(110, 93);
+            second.Margin = new Padding(3, 4, 3, 4);
             second.Name = "second";
-            second.Size = new Size(100, 32);
+            second.Size = new Size(114, 39);
             second.TabIndex = 1;
             // 
             // add
             // 
             add.Font = new Font("Segoe UI", 14F);
-            add.Location = new Point(96, 122);
+            add.Location = new Point(110, 163);
+            add.Margin = new Padding(3, 4, 3, 4);
             add.Name = "add";
-            add.Size = new Size(36, 33);
+            add.Size = new Size(41, 44);
             add.TabIndex = 2;
             add.Text = "+";
             add.UseVisualStyleBackColor = true;
@@ -70,74 +73,80 @@
             // subtract
             // 
             subtract.Font = new Font("Segoe UI", 14F);
-            subtract.Location = new Point(160, 122);
+            subtract.Location = new Point(183, 163);
+            subtract.Margin = new Padding(3, 4, 3, 4);
             subtract.Name = "subtract";
-            subtract.Size = new Size(36, 33);
+            subtract.Size = new Size(41, 44);
             subtract.TabIndex = 2;
             subtract.Text = "-";
             subtract.UseVisualStyleBackColor = true;
+            subtract.Click += subtract_Click;
             // 
             // multiply
             // 
             multiply.Font = new Font("Segoe UI", 14F);
-            multiply.Location = new Point(96, 180);
+            multiply.Location = new Point(110, 240);
+            multiply.Margin = new Padding(3, 4, 3, 4);
             multiply.Name = "multiply";
-            multiply.Size = new Size(36, 33);
+            multiply.Size = new Size(41, 44);
             multiply.TabIndex = 2;
             multiply.Text = "X";
             multiply.UseVisualStyleBackColor = true;
+            multiply.Click += multiply_Click;
             // 
             // divide
             // 
             divide.Font = new Font("Segoe UI", 14F);
-            divide.Location = new Point(160, 180);
+            divide.Location = new Point(183, 240);
+            divide.Margin = new Padding(3, 4, 3, 4);
             divide.Name = "divide";
-            divide.Size = new Size(36, 33);
+            divide.Size = new Size(41, 44);
             divide.TabIndex = 2;
             divide.Text = "/";
             divide.UseVisualStyleBackColor = true;
+            divide.Click += divide_Click;
             // 
             // result
             // 
             result.BackColor = SystemColors.ControlLightLight;
             result.Font = new Font("Segoe UI", 14F);
-            result.Location = new Point(94, 241);
+            result.Location = new Point(99, 332);
             result.Name = "result";
-            result.Size = new Size(100, 23);
+            result.Size = new Size(395, 31);
             result.TabIndex = 3;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(17, 32);
+            label1.Location = new Point(19, 43);
             label1.Name = "label1";
-            label1.Size = new Size(60, 15);
+            label1.Size = new Size(75, 20);
             label1.TabIndex = 4;
             label1.Text = "Number 1";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(17, 81);
+            label2.Location = new Point(19, 108);
             label2.Name = "label2";
-            label2.Size = new Size(60, 15);
+            label2.Size = new Size(75, 20);
             label2.TabIndex = 5;
             label2.Text = "Number 2";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(17, 249);
+            label3.Location = new Point(19, 332);
             label3.Name = "label3";
-            label3.Size = new Size(39, 15);
+            label3.Size = new Size(49, 20);
             label3.TabIndex = 6;
             label3.Text = "Result";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(227, 290);
+            ClientSize = new Size(517, 387);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -148,6 +157,7 @@
             Controls.Add(add);
             Controls.Add(second);
             Controls.Add(first);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
